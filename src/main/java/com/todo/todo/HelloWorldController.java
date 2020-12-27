62f7b1012bbfe6b883d5412b841bfc6e69e0b343
ritvij_saxena <saxenaritvij@gmail.com>
2020-12-27 14:41:45 -0600

    Controller. Bean creation and Path Variable usage.

--- src/main/java/com/todo/todo/HelloWorldController.java
+++ src/main/java/com/todo/todo/HelloWorldController.java
@@ -1,4 +1,27 @@
 package com.todo.todo;
 
+import org.springframework.web.bind.annotation.*;
+
+//Controller
+@RestController
 public class HelloWorldController {
+    // GET
+    // URI - /hello-world
+    //method - "Hello World"
+
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s",name));
+    }
 }
