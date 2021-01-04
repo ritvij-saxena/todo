c16432b074a17e5afac97f8b48f76a54a1312955
ritvij_saxena <saxenaritvij@gmail.com>
2021-01-04 11:49:04 -0600

    Swagger Documentation [update].

--- src/main/java/com/todo/todo/user/User.java
+++ src/main/java/com/todo/todo/user/User.java
@@ -1,17 +1,22 @@
 package com.todo.todo.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
-
+@ApiModel(description = "All details about the user")
 public class User {
     private Integer id;
 
     @Size(min=2, message = "Name should have at least 2 characters")
+    @ApiModelProperty(notes = "Name should have at least 2 characters")
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "Birth date should in the past")
     private Date birthDate;
 
     public User(Integer id, String name, Date birthDate) {
