1858494c5a88d429bfa245a3d7fd5967de2e748f
ritvij_saxena <saxenaritvij@gmail.com>
2021-01-04 10:55:43 -0600

    Simplification of previous commits.

--- src/main/java/com/todo/todo/helloworld/HelloWorldController.java
+++ src/main/java/com/todo/todo/helloworld/HelloWorldController.java
@@ -2,7 +2,9 @@ package com.todo.todo.helloworld;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.LocaleContextResolver;
 
 import java.util.Locale;
 
@@ -32,7 +34,7 @@ public class HelloWorldController {
     }
 
     @GetMapping(path = "/hello-world-internationalized")
-    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
-        return messageSource.getMessage("good.morning.message", null, locale);
+    public String helloWorldInternationalized() {
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
